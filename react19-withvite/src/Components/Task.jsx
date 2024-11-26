10cca28500ab909e3e4c84db931947c8973f7cce
Rupesh Pal <rupeshpal49@gmail.com>
2024-11-27 01:35:41 +0530

    state example

--- react19-withvite/src/Components/Task.jsx
+++ react19-withvite/src/Components/Task.jsx
@@ -1,15 +1,13 @@
+import { useState } from "react";
 import "./Style.css";
 export function Addphoto() {
+  const [greet, setGreet] = useState(0);
   return (
     <div>
       {/* <img src="\src\assets\krishna.jpg" alt="img-break" /> */}
       <img src="/src/assets/krishna.jpg" alt="img-break" />
-      <h1>Hello</h1>
-      <button onClick={clickMe}>Click Me</button>
+      <h1>Hello {greet}</h1>
+      <button onClick={() => setGreet(greet + 1)}>Click Me</button>
     </div>
   );
-}
-
-function clickMe() {
-  alert("I am clicked");
 }
