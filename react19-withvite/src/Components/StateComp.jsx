9ac996cfb4911f70b61a9d655d090fa42e956643
Rupesh Pal <rupeshpal49@gmail.com>
2024-11-27 23:39:33 +0530

    Add multiple condition in react

--- react19-withvite/src/Components/StateComp.jsx
+++ react19-withvite/src/Components/StateComp.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function StateChange() {
   const [ascendingOrder, setAscendingOrder] = useState(0);
+  const [higher, setHigher] = useState(0);
 
   return (
     <div>
@@ -18,6 +19,27 @@ function StateChange() {
       >
         Decrease Me
       </button>
+      <h3>{higher}</h3>
+      <button onClick={() => setHigher(higher + 1)}>Higher</button>
+      {higher == 0 ? (
+        <h1>Condition 0</h1>
+      ) : higher == 1 ? (
+        <h1>Condition 1</h1>
+      ) : higher == 2 ? (
+        <h1>Condition 2</h1>
+      ) : higher == 3 ? (
+        <h1>Condition 3 </h1>
+      ) : higher == 4 ? (
+        <h1>Condition 4</h1>
+      ) : higher == 5 ? (
+        <h1>Condition 5</h1>
+      ) : higher == 6 ? (
+        <h1>Condition 6</h1>
+      ) : higher == 7 ? (
+        <h1>Condition 7</h1>
+      ) : (
+        <h1>Wrong entry</h1>
+      )}
     </div>
   );
 }
